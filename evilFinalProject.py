d0ac2f72e58800aaaaf9d359e079c46e770af44d
Daniel Maciejewski <OfficialDanMaciejewski@gmail.com>
2021-04-24 15:55:17 -0500

    initial GUI setup

--- evilFinalProject.py
+++ evilFinalProject.py
@@ -1,2 +1,10 @@
+import os, configparser
+from tkinter import *
 #Ethical Hacking Python Application
-print("Hello Team")
+
+#GUI Rigging
+
+window =Tk() #create tkinter GUI object
+window.title("Group 1 Final Project")#Project Title
+window.geometry('1900x1200')#Set size of the pane
+
